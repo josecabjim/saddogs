1fe3aa0fdd6b4c28e470881d804d71f721166436
Jose Caballero <caballero.jimenez.jose@gmail.com>
2026-02-22 13:14:56 +0000

    adds workflow cron job

--- packages/saddogs-scrape/saddogs_scrape/saddogs_scrape/spiders/census.py
+++ packages/saddogs-scrape/saddogs_scrape/saddogs_scrape/spiders/census.py
@@ -1,5 +1,26 @@
+import os
+
 import scrapy
 import datetime
+from supabase import create_client
+
+# TODO mapping should be in a config file
+mapping = {
+    "No Canario": "no_canario",
+    "El Hierro": "el_hierro",
+    "Fuerteventura": "fuerteventura",
+    "Gran Canaria": "gran_canaria",
+    "La Gomera": "la_gomera",
+    "La Palma": "la_palma",
+    "Lanzarote": "lanzarote",
+    "Tenerife": "tenerife",
+}
+
+
+def str_to_int(s: str) -> int:
+    s = s.replace(".", "")
+    return int(s)
+
 
 class CensusSpider(scrapy.Spider):
     name = "census"
@@ -9,18 +30,26 @@ class CensusSpider(scrapy.Spider):
         self.table_key_islands = "Islas"
         self.table_key_dogs = "Perros"
 
+        # TODO use separate database module
+        url = os.environ["SUPABASE_URL"]
+        key = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+        self.supabase = create_client(url, key)
+
     def parse_header(self, response) -> list[str]:
         header_top = response.css("table thead th span::text").get()
         header_rest = response.css("table thead th::text").getall()
         header = [header_top] + header_rest
 
         if self.table_key_islands not in header:
-            raise ValueError(f"islands_table_key: {self.table_key_islands} not found in table header")
+            raise ValueError(
+                f"islands_table_key: {self.table_key_islands} not found in table header"
+            )
         if self.table_key_dogs not in header:
-            raise ValueError(f"dog_table_key: {self.table_key_dogs} not found in table header")
-        
-        return header
+            raise ValueError(
+                f"dog_table_key: {self.table_key_dogs} not found in table header"
+            )
 
+        return header
 
     def parse(self, response):
         header = self.parse_header(response)
@@ -39,13 +68,27 @@ class CensusSpider(scrapy.Spider):
         date = dict(
             year=datetime.date.today().year,
             month=datetime.date.today().month,
-            day=datetime.date.today().day
+            day=datetime.date.today().day,
         )
-        census_data = {island: n for island, n in zip(islands, dogs)}
+        data_census = {island: n for island, n in zip(islands, dogs)}
+        data_db = {
+            key_db: str_to_int(data_census[key_census])
+            for key_census, key_db in mapping.items()
+        }
 
-        data = dict(
-            date=date,
-            data=census_data
-        )
+        # TODO change date format?
+        data = dict(**data_db, **date)
 
-        yield data
+        # TODO use separate database module
+        try:
+            response = (
+                self.supabase.table("census")
+                .upsert(data, on_conflict="year,month,day")
+                .execute()
+            )
+
+            self.logger.info(f"Upsert successful: {response.data}")
+
+        except Exception as e:
+            self.logger.error(f"Upsert failed: {e}")
+            raise
