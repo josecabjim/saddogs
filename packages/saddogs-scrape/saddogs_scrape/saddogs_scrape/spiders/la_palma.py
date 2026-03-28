4b7cb1b86d0824103f4659123d5fa7c437d29c2c
Jose Caballero <caballero.jimenez.jose@gmail.com>
2026-03-28 11:29:40 +0000

    test failure

--- packages/saddogs-scrape/saddogs_scrape/saddogs_scrape/spiders/la_palma.py
+++ packages/saddogs-scrape/saddogs_scrape/saddogs_scrape/spiders/la_palma.py
@@ -7,6 +7,6 @@ class LaPalmaBenawara(CountSpider):
     rescue_name = "Benawara"
     island = "La Palma"
 
-    start_urls = ["https://benawara.es/en_GB/adopta"]
+    start_urls = ["https://benafwara.es/en_GB/adopta"]
 
     selector = "div.s_col_no_bgcolor.pt16.pb16.col-lg-3, div.s_col_no_bgcolor.o_grid_item.g-col-lg-3.g-height-14.col-lg-3"
